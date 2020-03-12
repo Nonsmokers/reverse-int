fdd705985c0b48412e21826089a2c493bef6775e
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-03-12 09:07:52 +0300

    feat: resolve task 'reverse'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let positive = Math.abs(n);
+    let str = positive.toString();
+    let newStr = str.split("").reverse().join("");
+     return +newStr;
 }
